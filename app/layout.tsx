2f501620877ff60b62760be750473741657d58b9
Miguel Bilenky <bilenkymiguel@gmail.com>
2026-03-22 13:14:59 -0300

    Adiciona CartProvider no app layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
+import Providers from "./providers";
 
 export const metadata: Metadata = {
   title: "D’Outro Lado",
@@ -13,7 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body>{children}</body>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
